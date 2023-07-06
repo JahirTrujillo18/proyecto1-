1094dc7127d3d96c9738eb0670dd9a81346d89a1
JahirTrujillo18 <jtrujillovilcherrez244@gmail.com>
2023-07-06 08:44:12 -0500

    actuliazar

--- app/src/main/java/com/example/proyecto_final/MainActivity.java
+++ app/src/main/java/com/example/proyecto_final/MainActivity.java
@@ -2,42 +2,52 @@ package com.example.proyecto_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    Button botonlogin;
+    EditText user,pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public class mainActivity extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+        user=findViewById(R.id.usuario);
+        pass=findViewById(R.id.contraseña);
+        botonlogin=findViewById(R.id.boton);
 
-            Toolbar horario = findViewById(R.id.myToolbar);
-            setSupportActionBar(horario);
-            getSupportActionBar().setTitle("");
+        botonlogin=(Button)findViewById(R.id.boton);
 
-        }
+        botonlogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (user.getText().toString().equals("user") && pass.getText().toString().equals("123")) {
 
-        private void setSupportActionBar(Toolbar horario) {
-        }
+                    Intent intent = new Intent(MainActivity.this,MainActivity2.class);
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            MenuInflater inflater = getMenuInflater();
-            inflater.inflate(R.menu.mymenu, menu);
-            return true;
-        }
+                    startActivity(intent);
 
-       
-    }
+                    //Toast.makeText(MainActivity.this, "Is OK", Toast.LENGTH_LONG).show();
 
-}
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Credenciales incorrectas", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+
+
+
+    }}
--- app/src/main/java/com/example/proyecto_final/MainActivity2.java
+++ app/src/main/java/com/example/proyecto_final/MainActivity2.java
@@ -1,4 +1,36 @@
 package com.example.proyecto_final;
 
-public class MainActivity2 {
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar horario = findViewById(R.id.myToolbar);
+        setSupportActionBar(horario);
+        getSupportActionBar().setTitle("");
+
+    }
+
+    private void setSupportActionBar(Toolbar horario) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mymenu, menu);
+        return true;
+    }
+
+
 }
+
+
