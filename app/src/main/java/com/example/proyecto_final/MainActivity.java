a4205a71ecc57c44fd04cd1a83c91bfe9a0db32d
JahirTrujillo18 <jtrujillovilcherrez244@gmail.com>
2023-07-06 10:20:10 -0500

    actuliazar

--- app/src/main/java/com/example/proyecto_final/MainActivity.java
+++ app/src/main/java/com/example/proyecto_final/MainActivity.java
@@ -1,17 +1,12 @@
 package com.example.proyecto_final;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
 public class MainActivity extends Activity {
 
@@ -21,7 +16,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.usuario_main);
 
         user=findViewById(R.id.usuario);
         pass=findViewById(R.id.contraseña);
